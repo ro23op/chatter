0efcf3d5e9cabf622eba38faf2c2e44e64f9998e
ro23op <rohannoronha8@gmail.com>
2025-03-20 13:42:41 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,13 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import {app, server} from './lib/socket.js'
 
+import path, { dirname } from 'path';
+
 
 dotenv.config()
 const PORT = process.env.PORT
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -22,6 +26,13 @@ app.use(express.json());
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../../frontend","dist","index.html"))
+    })
+}
 
 server.listen(PORT,()=>{
     console.log("Server is running on port "+ PORT)
